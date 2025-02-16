29f9c3dfecbb40d50c3f160275759778698ee098
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2025-02-16 12:03:03 +0200

    Update get-pip.sh

--- usr/local/bin/get-pip.sh
+++ usr/local/bin/get-pip.sh
@@ -2,6 +2,18 @@ echo -e "\e[34m|----------------------------------------------------------------
 echo -e "\e[36m|\e[32m                  Welcome Thanks For Using Get Imports                  \e[36m|\e[34m"
 echo -e "|------------------------------------------------------------------------|\e[0m"
 
+dev_loaded_packages=()
+current_loaded_packages=()
+
+dev_packages=()
+# what used now for both and work as native bash and same method main for both generate command install from dev by process it files python
+current_packages=()
+# right now same prod can handle both
+current_required_pip=()
+pip_install_command=""
+
+
+
 # handle inputs togther as it simple input html input "your input"
 function input() {
    if [ $1 = 'filepath' ]; then
@@ -10,6 +22,9 @@ function input() {
    elif [ $1 = 'last_imported_spliter' ]; then
       read -p 'Enter last imported line to get what after it or empty for all' last_imported_spliter
       echo "$last_imported_spliter"
+   elif [ $1 = 'pip_command_needed' ]; then
+      read -p 'Do you need get-pip display pip install command with all packages on this machine for speacfied file with the versions (usally do that on exporter like export command to done on prod) Continue (yes/no)?: ' pip_command_needed
+      echo "$pip_command_needed"
    elif [ $1 = 'confirm_continue' ]; then
       read -p 'Do you Confirm Continue (yes/no)?: ' confirm
       echo "$confirm"
@@ -19,7 +34,6 @@ function input() {
 error_msg() {
     echo -e "\e[31m$1\e[0m"  # Print in red
 }
-
 
 # return pip package or empty
 function get_pip_arg() {
@@ -31,10 +45,7 @@ function get_pip_arg() {
    fi
 }
 
-dev_packages=()
-prod_loaded=()
-prod_packages=()
-# for development server
+
 
 
 function main() {
@@ -66,7 +77,7 @@ function main() {
          # now this pip relative package not need pip or target package need pip
          pip_package=$(get_pip_arg "$line")
          if [[ "$pip_package" != '' ]]; then
-             prod_loaded+=("$pip_package")
+             current_loaded_packages+=("$pip_package")
              echo $pip_package
          fi
       fi
@@ -74,20 +85,25 @@ function main() {
    confirm_continue=$(input 'confirm_continue')
    if [ "$confirm_continue" = 'yes' ];
    then
-      for package in "${prod_loaded[@]}"; do
+      for package in "${current_loaded_packages[@]}"; do
          #echo pip show "$package" &>/dev/null
          package_version="$(pip show "$package" | grep -i "^Version:" | awk "{print $2}")"
          if [ -n "$package_version" ]; then
-             prod_packages+=("$package ($package_version)")
+             current_packages+=("$package ($package_version)")
+             current_required_pip+=("$package==$package_version")
              echo -e ""$package"=="$package_version""
              echo -e "\e[34m '$package' installed with version "$package_version" \e[0m"
          else
-             prod_packages+=("$package (0)")
+             current_packages+=("$package (0)")
             echo -e "\e[31mPackage '$package' is not installed.\e[0m"
          fi
       done
       echo 'going to start'
-      echo "Updated dev_packages: ${prod_packages[@]}"
+      pip_install_command="pip install $(IFS=' && '; echo "${current_required_pip[*]}")"
+      echo -e "\e[32mPIP_____________________PIP"
+      echo "$pip_install_command"
+      echo "PIP_____________________PIP\e[0"
+      echo "Updated current_packages: ${current_packages[@]}"
    else
       echo 'ok will not started ...'
    fi
